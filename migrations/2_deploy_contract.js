0308a6513c6261159a3080813dbce25e7752b7a4
Fauzan Lubis <fauzanfauh45@gmail.com>
2021-12-19 22:02:10 +0700

    ODAO Deployer Script

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,9 @@
-const MaDApps = artifacts.require("MaDApps");
+const ODAOToken = artifacts.require("ODAOToken");
+const ODAOIdentity = artifacts.require("ODAOIdentity");
 
-module.exports = function (deployer) {
-    deployer.deploy(MaDApps);
+module.exports = async function (deployer, network, accounts) {
+    await deployer.deploy(ODAOToken);
+    const ODAOTokenInstance = await ODAOToken.deployed();
+
+    await deployer.deploy(ODAOIdentity, ODAOTokenInstance.address, 1_000);
 };
